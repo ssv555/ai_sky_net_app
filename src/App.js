51b4e90f6f3879383327dbd5460ffd203ba3afd8
ssv555 <ssv555@mail.ru>
2025-03-27 20:49:50 +0300

    fix bug in App.jsx

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+const tg = window.Telegram.WebApp;
 
 function App() {
 
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="App">
-      work
+      Body of the app
     </div>
   );
 }

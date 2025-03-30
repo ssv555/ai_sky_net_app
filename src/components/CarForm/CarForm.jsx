99f638944c109dbdd7437d6995ed7b31939bda31
ssv555 <ssv555@mail.ru>
2025-03-30 16:46:41 +0300

    debug show MainButton 6

--- src/components/CarForm/CarForm.jsx
+++ src/components/CarForm/CarForm.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { WebApp } from "@twa-dev/sdk";
 import { useState, useEffect, useCallback } from "react";
 import { useTelegram } from "../../hooks/useTelegram";
-import { WebApp } from "@twa-dev/sdk";
 import "./CarForm.css";
 import "../../styles/common.css";
 
 const MAX_NAME_LENGTH = 255;
 const MAX_PRICE = 1000000000;
+const MainButton = useTelegram().MainButton;
 
 const CarForm = () => {
   const [carName, setCarName] = useState("");
@@ -14,76 +15,75 @@ const CarForm = () => {
   const [carModel, setCarModel] = useState("0");
   const { sendDataToServer } = useTelegram();
 
-  const onSendData = useCallback(() => {
-    if (!WebApp) {
-      console.error("Telegram WebApp не инициализирован");
-      return;
-    }
-    /*
-    const trimmedName = carName.trim();
-    if (!trimmedName) {
-      WebApp.showPopup({
-        title: "Ошибка",
-        message: "Пожалуйста, введите название автомобиля",
-        buttons: [{ type: "ok" }],
-      });
-      return;
-    }
-    if (trimmedName.length > MAX_NAME_LENGTH) {
-      WebApp.showPopup({
-        title: "Ошибка",
-        message: `Название не должно превышать ${MAX_NAME_LENGTH} символов`,
-        buttons: [{ type: "ok" }],
-      });
-      return;
-    }
-    const price = Number(carPrice);
-    if (!carPrice.trim() || isNaN(price)) {
-      WebApp.showPopup({
-        title: "Ошибка",
-        message: "Пожалуйста, введите корректную цену",
-        buttons: [{ type: "ok" }],
-      });
-      return;
-    }
-    if (price < 0) {
-      WebApp.showPopup({
-        title: "Ошибка",
-        message: "Цена не может быть отрицательной",
-        buttons: [{ type: "ok" }],
-      });
-      return;
-    }
-    if (price > MAX_PRICE) {
-      WebApp.showPopup({
-        title: "Ошибка",
-        message: `Цена не может превышать ${MAX_PRICE}`,
-        buttons: [{ type: "ok" }],
-      });
-      return;
-    }
-    */
-    const data = {
-      carName: trimmedName,
-      carPrice: price,
-      carModel: Number(carModel),
-    };
+  // const onSendData = useCallback(() => {
+  //   if (!WebApp) {
+  //     console.error("Telegram WebApp не инициализирован");
+  //     return;
+  //   }
+  //   /*
+  //   const trimmedName = carName.trim();
+  //   if (!trimmedName) {
+  //     WebApp.showPopup({
+  //       title: "Ошибка",
+  //       message: "Пожалуйста, введите название автомобиля",
+  //       buttons: [{ type: "ok" }],
+  //     });
+  //     return;
+  //   }
+  //   if (trimmedName.length > MAX_NAME_LENGTH) {
+  //     WebApp.showPopup({
+  //       title: "Ошибка",
+  //       message: `Название не должно превышать ${MAX_NAME_LENGTH} символов`,
+  //       buttons: [{ type: "ok" }],
+  //     });
+  //     return;
+  //   }
+  //   const price = Number(carPrice);
+  //   if (!carPrice.trim() || isNaN(price)) {
+  //     WebApp.showPopup({
+  //       title: "Ошибка",
+  //       message: "Пожалуйста, введите корректную цену",
+  //       buttons: [{ type: "ok" }],
+  //     });
+  //     return;
+  //   }
+  //   if (price < 0) {
+  //     WebApp.showPopup({
+  //       title: "Ошибка",
+  //       message: "Цена не может быть отрицательной",
+  //       buttons: [{ type: "ok" }],
+  //     });
+  //     return;
+  //   }
+  //   if (price > MAX_PRICE) {
+  //     WebApp.showPopup({
+  //       title: "Ошибка",
+  //       message: `Цена не может превышать ${MAX_PRICE}`,
+  //       buttons: [{ type: "ok" }],
+  //     });
+  //     return;
+  //   }
+  //   */
+  //   const data = {
+  //     carName: trimmedName,
+  //     carPrice: price,
+  //     carModel: Number(carModel),
+  //   };
 
-    WebApp.showPopup({
-      title: "data",
-      message: JSON.stringify(data),
-      buttons: [{ type: "ok" }],
-    });
+  //   // WebApp.showPopup({
+  //   //   title: "data",
+  //   //   message: JSON.stringify(data),
+  //   //   buttons: [{ type: "ok" }],
+  //   // });
 
-    sendDataToServer(data);
-  }, [carName, carPrice, carModel, sendDataToServer]);
+  //   sendDataToServer(data);
+  // }, [carName, carPrice, carModel, sendDataToServer]);
 
   useEffect(() => {
     WebApp.showAlert(
       `carName: ${carName}\ncarPrice: ${carPrice}\ncarModel: ${carModel}`
     );
 
-    const MainButton = useTelegram().MainButton;
     MainButton.setText("Сохранить");
     MainButton.setBackgroundColor("#2481cc");
     MainButton.onClick(onSendData);
@@ -93,41 +93,42 @@ const CarForm = () => {
     };
   }, [onSendData, carName, carPrice, carModel]);
 
-  useEffect(() => {
-    /*
-    if (!WebApp) return;
+  // useEffect(() => {
+  //   /*
+  //   if (!WebApp) return;
 
-    try {
-      const trimmedName = carName.trim();
-      const price = Number(carPrice);
-      const isValidPrice = !isNaN(price) && price >= 0 && price <= MAX_PRICE;
+  //   try {
+  //     const trimmedName = carName.trim();
+  //     const price = Number(carPrice);
+  //     const isValidPrice = !isNaN(price) && price >= 0 && price <= MAX_PRICE;
 
-      WebApp.showPopup({
-        title: "Отладочная информация",
-        message: `trimmedName: ${trimmedName}\nisValidPrice: ${isValidPrice}\ncarModel: ${carModel}`,
-        buttons: [{ type: "ok" }],
-      });
+  //     WebApp.showPopup({
+  //       title: "Отладочная информация",
+  //       message: `trimmedName: ${trimmedName}\nisValidPrice: ${isValidPrice}\ncarModel: ${carModel}`,
+  //       buttons: [{ type: "ok" }],
+  //     });
 
-      if (trimmedName && isValidPrice && carModel !== "0") {
-        console.log("Показ кнопки");
-        WebApp.MainButton.setText("Сохранить");
-        WebApp.MainButton.setBackgroundColor("#2481cc");
-        WebApp.MainButton.show();
-      } else {
-        console.log("Скрытие кнопки");
-        WebApp.MainButton.hide();
-      }
-    } catch (error) {
-      console.error("Ошибка при управлении MainButton:", error);
-    }
-      */
-  }, [WebApp, carName, carPrice, carModel]);
+  //     if (trimmedName && isValidPrice && carModel !== "0") {
+  //       console.log("Показ кнопки");
+  //       WebApp.MainButton.setText("Сохранить");
+  //       WebApp.MainButton.setBackgroundColor("#2481cc");
+  //       WebApp.MainButton.show();
+  //     } else {
+  //       console.log("Скрытие кнопки");
+  //       WebApp.MainButton.hide();
+  //     }
+  //   } catch (error) {
+  //     console.error("Ошибка при управлении MainButton:", error);
+  //   }
+  //     */
+  // }, [WebApp, carName, carPrice, carModel]);
 
   const changeCarName = (e) => {
     const value = e.target.value;
     if (value.length <= MAX_NAME_LENGTH) {
       setCarName(value);
     }
+    WebApp.showAlert(`carName: ${carName}`);
   };
 
   const changeCarPrice = (e) => {
@@ -135,10 +136,12 @@ const CarForm = () => {
     if (value === "" || /^\d+$/.test(value)) {
       setCarPrice(value);
     }
+    WebApp.showAlert(`carPrice: ${carPrice}`);
   };
 
   const changeCarModel = (e) => {
     setCarModel(e.target.value);
+    WebApp.showAlert(`carModel: ${carModel}`);
   };
 
   return (

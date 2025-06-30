2e3b5c00836c44fc8c6432b0c7869aa40ceae6e6
ssv555 <ssv555@mail.ru>
2025-07-01 01:20:32 +0300

    fix config, add vite.svg icon, and some build problems.

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,20 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/",
-  server: {
-    port: 31000,
-    open: true,
-    host: true,
-    allowedHosts: ["v570907.hosted-by-vdsina.ru"],
-  },
-  build: {
-    outDir: "dist",
-    sourcemap: true,
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_PATH || "/",
+    server: {
+      port: 31000,
+      open: true,
+      host: true,
+      allowedHosts: ["v570907.hosted-by-vdsina.ru"],
+    },
+    build: {
+      outDir: "dist",
+      sourcemap: true,
+    },
+  };
 });
